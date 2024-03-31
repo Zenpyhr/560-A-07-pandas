72c1045935eb0e2743356fde8eff0ea0ff793b1f
Zenpyhr <boyangzhu03@gmail.com>
2024-03-30 23:30:33 -0400

    0 header

--- roster.py
+++ roster.py
@@ -1,7 +1,10 @@
 #https://goheels.com/sports/mens-basketball/roster
 
-roster = ["Bacot", "Davis", "Cadeau"]
 
-for player in roster:
-    print(roster)
+import pandas as pd
+
+
+roster = ["Bacot", "Davis", "Cadeau"]
+data = pd.DataFrame(roster)
+print(data)
 
